142efec482639863ec64d9004d7d008c4907c8e1
Albert Pang <albert.pang@me.com>
2015-08-16 12:52:04 +0800

    Re-created project/solution with visual studio

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -1,20 +1,41 @@
-﻿namespace FSound
+﻿namespace FSound.AssemblyInfo
+
 open System.Reflection
 open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [<assembly: AssemblyTitle("FSound")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("")>]
-[<assembly: AssemblyCopyright("panga")>]
+[<assembly: AssemblyProduct("FSound")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
 [<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
 
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("550f3f6e-0b2e-41ee-a5cd-b9e2b80a1b37")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
 
-//[<assembly: AssemblyDelaySign(false)>]
-//[<assembly: AssemblyKeyFile("")>]
-
-()
+do
+    ()
