4da7595ee3b59556c8d3d54ea551fe23da5450a6
Albert Pang <albert.pang@me.com>
2015-08-22 11:06:25 +0800

    Removed namespace and module in test/demo script Added seqToPoint for charting Added testReadWavFile (but it hangs with large input for now)

--- SignalTest.fsx
+++ SignalTest.fsx
@@ -2,22 +2,31 @@
 // for more guidance on F# programming.
 
 #I @"packages\FSharp.Charting.0.90.12\lib\net40"
+#I @"packages\MathNet.Numerics.3.7.0\lib\net40"
 #r @"FSharp.Charting.dll"
+#r @"MathNet.Numerics.dll"
 #load "Signal.fs"
+#load "IO.fs"
 
-namespace FSound
+open FSound.Signal
+open FSound.IO
+open FSharp.Charting
 
-module SignalTest =
+let seqToPoint s =
+  s |> Seq.mapi (fun i x -> (i,x))
 
-  open FSound.Signal
-  open FSharp.Charting
+// Define your library scripting code here
+let testSinewave =
+  let start = 0.0
+  let endTime = 0.01
+  let sf = 44100.0
+  let s = sinewave 0.8 440.0 0.0 44100.0 0.0 2.0
+  let i = [start..(1.0/sf)..endTime]
+  Seq.zip i s |> Chart.Line
 
-  // Define your library scripting code here
-  let testSinewave =
-    let start = 0.0
-    let endTime = 0.01
-    let sf = 44100.0
-    let s = sinewave 0.8 440.0 0.0 44100.0 0.0 2.0
-    let i = [start..(1.0/sf)..endTime]
-    Seq.zip i s |> Chart.Line
-
+let testReadWavFile =
+  let wav = readWavFile @"chimes.wav"
+  fst wav.Data 
+  |> fft 
+  |> Array.mapi (fun i x -> (i, x)) 
+  |> Chart.Point
