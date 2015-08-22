5ba97eceff5e5378ea96e1a19d843152e3a12ca8
Albert Pang <albert.pang@me.com>
2015-08-22 10:50:25 +0800

    Changed sound file data to be list of floats which is expected to be easier to process later on Changed to use byte string to compare chunk ids saving extra conversions to string Splitted the data into left channel and right channel

--- IO.fs
+++ IO.fs
@@ -4,19 +4,24 @@ module IO =
 
   type SoundFile (samplingRate: float, 
                   numChannels: int, 
-                  data: seq<byte>, 
+                  data: (float list * float list),
                   isPCM: bool) =
     member f.SamplingRate = samplingRate
     member f.Data = data
     member f.IsPCM = isPCM
     member f.NumChannels = numChannels
-
+  
+  /// <summary>Reads a PCM wave file and return a SoundFile object</summary>
+  /// <param name="path">Path of the file</param>
+  /// <returns>a SoundFile object representing the data in the wav file. If the
+  /// file has only one channel, then the first of the tuple in Data contains
+  /// the data and the second of the tuple is empty</returns>
   let readWavFile (path: string) =
       use fileStream = new System.IO.FileStream( path, System.IO.FileMode.Open )
       use reader = new System.IO.BinaryReader(fileStream)
       // read chunk id - 4 bytes
-      let id = System.Text.Encoding.Default.GetString(reader.ReadBytes(4))
-      if id.ToString() <> "RIFF" then
+      let id = reader.ReadBytes(4)
+      if id <> "RIFF"B then
         failwith "Not a WAV file"
       // ignore next 12 bytes - chunk size: 4, format: 4, subChunkId: 4
       reader.ReadBytes(12) |> ignore
@@ -35,10 +40,12 @@ module IO =
         let nExtraBytes = System.BitConverter.ToInt32(reader.ReadBytes(4), 0)
         reader.ReadBytes(nExtraBytes) |> ignore
 
-      let idChunk2 = System.Text.Encoding.Default.GetString(reader.ReadBytes(4))
-      if idChunk2 <> "data" then
+      let idChunk2 = reader.ReadBytes(4)
+      if idChunk2 <> "data"B then
         failwith "Incorrect format: chunk 2 id not 'data'"
-      let nBytes = System.BitConverter.ToInt32(reader.ReadBytes(4), 0)    
-      SoundFile (float samplingRate, int numChannels, reader.ReadBytes(nBytes),
-        (int audioFormat = 1))
+      let nBytes = System.BitConverter.ToInt32(reader.ReadBytes(4), 0)  
+      let data = Array.foldBack (fun x (l,r) -> ((float x)::r, l)) 
+                   (reader.ReadBytes(nBytes)) ([],[])
 
+      SoundFile (float samplingRate, int numChannels, data, 
+        (int audioFormat = 1))
