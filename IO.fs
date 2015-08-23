de7d36ad374458b423def45a69fe08370cdc532a
Albert Pang <albert.pang@me.com>
2015-08-23 18:46:51 +0800

    Added bit depth field to represent the number of bits per sample Reverted to return raw data in array of bytes rather than splitting in to channels (i.e. bit depth was not considered before when splitting into channels which is completely wrong)

--- IO.fs
+++ IO.fs
@@ -4,13 +4,15 @@ module IO =
 
   type SoundFile (samplingRate: float, 
                   numChannels: int, 
-                  data: (float list * float list),
+                  bitDepth: int,
+                  rawData: byte[],
                   isPCM: bool) =
     member f.SamplingRate = samplingRate
-    member f.Data = data
+    member f.BitDepth = bitDepth
+    member f.RawData = rawData
     member f.IsPCM = isPCM
     member f.NumChannels = numChannels
-  
+   
   /// <summary>Reads a PCM wave file and return a SoundFile object</summary>
   /// <param name="path">Path of the file</param>
   /// <returns>a SoundFile object representing the data in the wav file. If the
@@ -31,9 +33,14 @@ module IO =
       let numChannels = int(System.BitConverter.ToInt16(reader.ReadBytes(2), 0))
       let samplingRate = System.BitConverter.ToInt32(reader.ReadBytes(4), 0)
 
-      // ignore rest of chunk 1
-      reader.ReadBytes(sizeChunk1-8) |> ignore
+      // ignore byte rate: 4, block align: 2
+      reader.ReadBytes(6) |> ignore
+      let bitsPerSample = 
+        int(System.BitConverter.ToInt16(reader.ReadBytes(2), 0))
 
+      // ignore the rest of chunk 1
+      reader.ReadBytes(sizeChunk1-16) |> ignore
+      
       if int audioFormat <> 1 then
         // Non-PCM
         reader.ReadBytes(4) |> ignore
@@ -44,11 +51,6 @@ module IO =
       if idChunk2 <> "data"B then
         failwith "Incorrect format: chunk 2 id not 'data'"
       let nBytes = System.BitConverter.ToInt32(reader.ReadBytes(4), 0)  
-      let data = 
-        let raw = reader.ReadBytes(nBytes)
-        if numChannels = 1 then 
-          (Array.map (fun x -> float x) raw |> Array.toList, [])
-        else
-          Array.foldBack (fun x (l,r) -> ((float x)::r, l)) raw ([],[])
 
-      SoundFile (float samplingRate, numChannels, data, (audioFormat = 1))
+      SoundFile (float samplingRate, numChannels, bitsPerSample,
+        reader.ReadBytes(nBytes), (audioFormat = 1))
