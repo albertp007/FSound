3d2005e704b379f3a584e859b46b3e60449a164f
Albert Pang <albert.pang@me.com>
2015-08-27 23:04:25 +0800

    Turned readWavFile to be private, replaced by static member ReadWav Added Channels member which is an array of sequence of floats representing the samples for each channel

--- IO.fs
+++ IO.fs
@@ -2,23 +2,18 @@
 
 module IO =
 
-  type SoundFile (samplingRate: float, 
-                  numChannels: int, 
-                  bitDepth: int,
-                  rawData: byte[],
-                  isPCM: bool) =
-    member f.SamplingRate = samplingRate
-    member f.BitDepth = bitDepth
-    member f.RawData = rawData
-    member f.IsPCM = isPCM
-    member f.NumChannels = numChannels
-   
+  let private genIndices numChannels bytesPerSample channel lengthSamples =
+    let bytesPerChannel = bytesPerSample * numChannels
+    let lengthSamples' = lengthSamples/bytesPerChannel*bytesPerChannel
+    let offset = bytesPerSample*(channel%numChannels)
+    seq { offset..(bytesPerChannel)..(lengthSamples'-1+offset) }
+
   /// <summary>Reads a PCM wave file and return a SoundFile object</summary>
   /// <param name="path">Path of the file</param>
   /// <returns>a SoundFile object representing the data in the wav file. If the
   /// file has only one channel, then the first of the tuple in Data contains
   /// the data and the second of the tuple is empty</returns>
-  let readWavFile (path: string) =
+  let private readWavFile (path: string) =
       use fileStream = new System.IO.FileStream( path, System.IO.FileMode.Open )
       use reader = new System.IO.BinaryReader(fileStream)
       // read chunk id - 4 bytes
@@ -52,5 +47,28 @@ module IO =
         failwith "Incorrect format: chunk 2 id not 'data'"
       let nBytes = System.BitConverter.ToInt32(reader.ReadBytes(4), 0)  
 
-      SoundFile (float samplingRate, numChannels, bitsPerSample,
-        reader.ReadBytes(nBytes), (audioFormat = 1))
+      (float samplingRate, numChannels, bitsPerSample, reader.ReadBytes(nBytes),
+       (audioFormat = 1))
+
+  type SoundFile (samplingRate: float, 
+                  numChannels: int, 
+                  bitDepth: int,
+                  rawData: byte [],
+                  isPCM: bool) =
+    let read bytes n = match bitDepth with
+                       | 8 -> rawData.[n] |> int8 |> float
+                       | 16 -> float(System.BitConverter.ToInt16(rawData, n))
+                       | _ -> failwith "Bit depth must be either 8 or 16"
+    member f.SamplingRate = samplingRate
+    member f.BitDepth = bitDepth
+    member f.RawData = rawData
+    member f.IsPCM = isPCM
+    member f.NumChannels = numChannels
+    member f.Channels = [|
+      for i in [0..(numChannels-1)] ->
+      f.RawData
+      |> Array.length
+      |> genIndices numChannels (bitDepth/8) i
+      |> Seq.map (fun n -> read rawData n) 
+      |]
+    static member ReadWav (path:string) = readWavFile path |> SoundFile
