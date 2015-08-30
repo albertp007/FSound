d25d406754ce3e2db0de15c36a7906e9f6d64f11
Albert Pang <albert.pang@me.com>
2015-08-30 15:19:15 +0800

    Added generator functions for sinusoid, square, white noise, a crude model of the sound of waves, and a modulate function

--- Signal.fs
+++ Signal.fs
@@ -4,11 +4,39 @@ module Signal =
 
   open MathNet.Numerics.IntegralTransforms
 
-  let sinewave amp freq phase samplingFreq startT endT =
-    let t = seq [startT..(1.0/samplingFreq)..endT]
+  let private random = System.Random()
+
+  let generate sf t genFunc =
+    let t = seq [0.0..(1.0/sf)..t]
+    Seq.map genFunc t
+
+  let sinusoid a f ph t =
     let pi = System.Math.PI
-    let x t = amp * cos (2.0*pi*freq*t + phase)
-    Seq.map x t
+    a * cos (2.0*pi*f*t + ph)
+
+  let whiteNoise a (t:float) =
+    a * (random.NextDouble() - 0.5)
+
+  let square (a:float) (f:float) (t:float) =
+    let i = int ( 2.0 * f * t ) 
+    if i % 2 = 0 then a else -a
+    
+  let modulate (signal:float->float) (modulator:float->float) (t: float) =
+    let primary = signal t
+    (signal t) * (modulator t)
+
+  let sinusoidGenerator a f ph sf t =
+    sinusoid a f ph |> generate sf t
+
+  let whiteNoiseGenerator a sf t =
+    whiteNoise a |> generate sf t
+
+  let waveGenerator a f sf t =
+    modulate (whiteNoise a) (sinusoid 1.0 f 0.0)
+    |> generate sf t
+
+  let squareGenerator a f sf t =
+    square a f |> generate sf t
 
   let foldi f acc xs = 
     let rec foldi' f i acc xs =
