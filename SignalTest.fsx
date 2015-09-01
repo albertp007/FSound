e3877029460abfe4373429dbc9a53b5cb4479a2a
Albert Pang <albert.pang@me.com>
2015-09-01 19:05:50 +0800

    Added GPL 3.0 copyright notice

--- SignalTest.fsx
+++ SignalTest.fsx
@@ -1,6 +1,22 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
+﻿//
+// FSound - F# Sound Processing Library
+// Copyright (c) 2015  Albert Pang. All rights reserved.
+//
+// This file is a part of FSound
+//
+// FSound is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FSound is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 #I @"bin/Debug"
 #r @"FSound.dll"
 
