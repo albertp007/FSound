52976466320ef7adcffee9155cf3ea3c0e3e0322
Albert Pang <albert.pang@me.com>
2015-09-19 10:18:32 +0800

    Added plot module and plot freq function using FSharp.Charting

---
+++ Plot.fs
@@ -0,0 +1,24 @@
+﻿
+namespace FSound
+
+module Plot =
+
+  open FSharp.Charting
+  open FSound.Utilities
+
+  //
+  // <summary>Run FFT on signal and plot frequency content using FSharp.Charting
+  // </summary>
+  // <param name="signal">Sequence of floats representing the samples</param>
+  // <returns>unit</returns>
+  //
+  let plotFreq toFreq samples =
+    samples
+    |> fft
+    |> magnitudes
+    |> Seq.take toFreq
+    |> Chart.Point
+    |> fun c -> c.ShowChart()
+
+
+
